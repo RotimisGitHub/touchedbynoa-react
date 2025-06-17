4cf51a0ebdd81a851cada2654755359df6b1ba9a
“RotimisGitHub” <rotimijatto03@gmail.com>
2025-06-17 20:34:34 +0100

    added necessary dependencies 3

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,19 +1,19 @@
 import rootReducer from './root-reducer';
 import {configureStore} from "@reduxjs/toolkit";
-import logger from "redux-logger";
-
-
-const middleWares = [];
-
-if (process.env.REACT_APP_DEV === 'yes') {
-    middleWares.push(logger);
-}
+// import logger from "redux-logger";
+//
+//
+// const middleWares = [];
+//
+// if (process.env.REACT_APP_DEV === 'yes') {
+//     middleWares.push(logger);
+// }
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
-    }).concat(middleWares)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    //     serializableCheck: false,
+    // }).concat(middleWares)
 });
 
 
